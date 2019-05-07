cd3621007e32a22b25592ad41144e5febdc65661
adria quiñoa jimenez <aquinoa@elpuig.xeill.net>
2019-05-07 17:22:41 +0200

    uytuty

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        File file = new File("myfile");
+        File file = new File("COMPETICION");
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
         BufferedWriter bufferedReader = new BufferedReader(new FileReader(file));
