6ecbe3b314e2e121b1b516c95245fbef854cfd1c
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2019-05-07 16:52:08 +0200

    dsafdsa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	
-        Scanner scanner = new Scaner();
 
-        System.out.println("Hola");
-        int a = 0;
-
-        hfgh
-
-                hfghfghf
-    }
 }
