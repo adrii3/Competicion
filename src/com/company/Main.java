22ce2cc2be379dada776379b10f17a23cc406d43
adria quiñoa jimenez <aquinoa@elpuig.xeill.net>
2019-05-07 17:20:27 +0200

    uytuty

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args){
 
-        File file = new file("myfile");
-
-
+        File file = new File("myfile");
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter)
-
 
     }
 }
