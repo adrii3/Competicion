282119b51732b4adf7d2190b8bba5be43d06b8c7
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2019-05-07 17:31:30 +0200

    dsafdsa

---
+++ src/com/company/TeamsScreen.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TeamsScreen {
+
+    void show(){
+        System.out.println("MENU QUPOS");
+
+        // if opcion == 1
+    }
+
+
+    void inscribirEquipo(){
+        // pedir daots y guardar en fichero
+    }
+}
