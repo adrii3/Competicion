535096f52f6caf5d5ce2cca0c15c3b28d95bf5b1
Saidu Ousmane Sakera <ssakera10@hotmail.com>
2019-05-13 17:44:54 +0200

    screens

--- src/com/company/view/clasification/ResultsScreen.java
+++ src/com/company/view/clasification/ResultsScreen.java
@@ -2,4 +2,6 @@ package com.company.view.clasification;
 
 public class ResultsScreen {
 
+
+
 }
---
+++ src/com/company/view/team/DeleteTeamScreen.java
@@ -0,0 +1,6 @@
+package com.company.view.team;
+
+public class DeleteTeamScreen {
+
+    public void show()
+}
