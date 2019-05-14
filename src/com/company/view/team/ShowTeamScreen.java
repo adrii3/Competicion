28b45207855a26e40d07fb0d15159171c1a99504
adria quiñoa jimenez <aquinoa@elpuig.xeill.net>
2019-05-14 17:05:20 +0200

    FDAS

--- src/com/company/view/team/ShowTeamScreen.java
+++ src/com/company/view/team/ShowTeamScreen.java
@@ -1,9 +1,20 @@
 package com.company.view.team;
 
+import com.company.model.Team;
+
+import java.util.Scanner;
+
 public class ShowTeamScreen {
 
     public void show(){
+        Team team = new Team();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Que equipo quieres ver");
+        System.out.println("Introduzca el id del equipo que desea ver: ");
+        team.id = sc.nextInt();
+        //buscar equipo en el fichero
+
+        //Mostrar datos del equipo
+
     }
 }
