78e17e7baa667b79bfa9da9e9d496a8ca82e5c38
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2019-05-14 17:12:55 +0200

    dsafdsa

--- src/com/company/view/participant/DeleteParticipantScreen.java
+++ src/com/company/view/participant/DeleteParticipantScreen.java
@@ -1,8 +1,18 @@
 package com.company.view.participant;
 
+import java.util.Scanner;
+
 public class DeleteParticipantScreen {
 
     public void show(){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Elige al participante que quieres eliminar");
+
+        int id = sc.nextInt();
+        System.out.println(" id : " + id);
+
+        //eliminar partcipante id, . y flan
+
     }
 }
--- src/com/company/view/participant/ShowParticipantScreen.java
+++ src/com/company/view/participant/ShowParticipantScreen.java
@@ -1,10 +1,17 @@
 package com.company.view.participant;
 
+import java.util.Scanner;
+
 public class ShowParticipantScreen {
 
     public void show(){
+        Scanner sc=new Scanner(System.in);
 
         System.out.println("Que participantes quieres ver");
+        int id = sc.nextInt();
+        System.out.println(" id : " + id);
+
+        //mostrar datos participante id
 
     }
 }
