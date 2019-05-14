c372a043811f0acc78cef8086b321d2f81f7d889
Edgar Ordoñez <edgarordon@elpuig.xeill.net>
2019-05-14 16:22:25 +0200

    dsafdsa

---
+++ src/com/company/view/participant/ModifyParticipantScreen.java
@@ -0,0 +1,18 @@
+package com.company.view.participant;
+
+import java.util.Scanner;
+
+public class ModifyParticipantScreen {
+    public void show() {
+        System.out.println("Introduzca el id del participante que desea modificar");
+        Scanner sc = new Scanner(System.in);
+
+        String  nombre = sc.nextLine();
+        System.out.println(" ID : "+ id);
+
+        System.out.println("Que quieres hacer");
+
+        System.out.println("1.");
+
+    }
+}
--- src/com/company/view/team/DeleteTeamScreen.java
+++ src/com/company/view/team/DeleteTeamScreen.java
@@ -1,7 +1,12 @@
 package com.company.view.team;
 
+import java.sql.SQLOutput;
+
 public class DeleteTeamScreen {
 
+    public void show(){
+        System.out.println("Introduzca el id del equipo que desea borrar");
+    }
     public void show(){
         System.out.println("Borra un equipo");
 
